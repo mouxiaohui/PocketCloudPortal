7d5df686bf0da987948c7d8d333af6f7c0ea8ddf
xiaohuiii <18367652239@163.com>
2025-03-18 21:56:35 +0800

    feat: 实现所有文档文件列表

--- src/views/file/doc-file/index.vue
+++ src/views/file/doc-file/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    doc
+  <div class="mt-[30px]">
+    <file-table />
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import useStore from '@/stores'
+import FileTable from '@/components/file-table/index.vue'
+
+const { fileStore, breadcrumbStore } = useStore()
+
+onMounted(() => {
+  fileStore.setMultipleSelection([])
+  breadcrumbStore.clear()
+  fileStore.setSearchFlag(false)
+  fileStore.setFileTypes('3__,__4__,__5__,__6__,__10__,__11')
+  fileStore.setParentId('-1')
+  fileStore.loadFileList()
+})
+</script>
