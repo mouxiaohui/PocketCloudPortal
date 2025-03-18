4a9c43a82a4bee3062e4f0d5c8f7cf80d24b5e8e
xiaohuiii <18367652239@163.com>
2025-03-18 21:53:55 +0800

    feat: 实现所有音乐文件列表

--- src/views/file/music-file/index.vue
+++ src/views/file/music-file/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    music
+  <div class="mt-[30px]">
+    <file-table />
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import useStore from '@/stores'
+import FileTable from '@/components/file-table/index.vue'
+
+const { fileStore, breadcrumbStore } = useStore()
+
+onMounted(() => {
+  fileStore.setMultipleSelection([])
+  breadcrumbStore.clear()
+  fileStore.setSearchFlag(false)
+  fileStore.setFileTypes('8')
+  fileStore.setParentId('-1')
+  fileStore.loadFileList()
+})
+</script>
